308be6de4a7510849e166960420c6b5c95ce5a27
guilopesdev <lopesgui.pro@gmail.com>
2024-01-28 19:42:21 -0300

    Primeiros passos com Python

---
+++ testes_solo/main.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+peso = float(input('Digite seu peso: '))
+idade = int(input('Digite sua idade: '))
+
+print(nome)
+print(peso)
+print(idade)
+
+print(type(nome),type(peso),type(idade))
