b057c8018ddac51d28b504672de3edcbf2572fba
guilopesdev <lopesgui.pro@gmail.com>
2024-01-30 06:16:47 -0300

    Aulas 6 a 9
    Conversão de tipo de dados, variáveis e operações

---
+++ aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adição ', adicao)
+
+subtracao = 10 - 5
+print('Subtração ', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação ', multiplicacao)
+
+divisao = 10 / 3
+print('Divisão ', divisao)
+
+divisao_inteira = 10//3
+print('Divisão inteira ou sem casas decimais ', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação ', exponenciacao)
+
+modulo = 25 % 5 # resto da divisão
+print('Módulo ou o resto da divisão ', modulo)
