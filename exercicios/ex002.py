2371406b76c30290187dbb64dbfc4f68219a5569
guilopesdev <lopesgui.pro@gmail.com>
2024-02-05 05:57:58 -0300

    Ex002
    Inicio e primeira resolução de um dos três exercícios

---
+++ exercicios/ex002.py
@@ -0,0 +1,29 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+numero = input('Digite um número inteiro: ')
+
+try:
+    numero_int = int(numero)
+    if numero_int % 2 == 0:
+        print('Seu número digitado é par.')
+    else:
+        print('Seu número é impar.')
+except:
+    print('Esse não é um número inteiro')
+
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
