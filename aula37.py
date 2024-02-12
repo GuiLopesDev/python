97bb55451370610de3ad6adb7eab12788d5afe1b
guilopesdev <lopesgui.pro@gmail.com>
2024-02-12 06:00:57 -0300

    Create aula37.py
    Introdução ao uso de else dentro do while

---
+++ aula37.py
@@ -0,0 +1,16 @@
+'''
+while/else
+'''
+
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[1]
+
+    print(letra)
+    i += 1
+else:
+    print('Chegou no else')
+
+print('Acabou o while')
