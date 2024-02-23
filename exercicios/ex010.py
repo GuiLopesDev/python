6345ebeb497a072f0f7bc0e30d518aa4eeb9a549
guilopesdev <lopesgui.pro@gmail.com>
2024-02-23 06:29:49 -0300

    ex010
    Exercício finalizado.
    Lista de compras interativa, com opção de apagar, listar e inserir.

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -3,7 +3,7 @@ Faça uma lista de compras usando lista
 O usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista
 Não permita que o programa quebre com erros de índices inexistentes na lista.
 '''
-
+import os # Usado para limpar o terminal, facilitando a visualização do usuário 
 lista_compras = []
 
 while True:
@@ -14,21 +14,43 @@ while True:
     # Inserir produtos
 
     if opcao_selecionada == 'i':
+
+        os.system('clear')
+
         produto_adicionado = input('Insira o produto desejado: ')
         lista_compras.append(produto_adicionado)
     
     # Apagar produtos
 
     elif opcao_selecionada == 'a':
+        
         indice_apagar = input('Escolha o índice para apagar: ')
+
+        # Tratativa dos erros possíveis
+
         try:    
             lista_compras.pop(int(indice_apagar))
-        except:
-            print('Não foi possível apagar este índice')
+        except TypeError:
+            print('Por favor indique o indice usando número inteiro')
+        except IndexError:
+            print('Índice inexistente')
+        except Exception:
+            print('Erro desconhecido')
+
+        os.system('clear')
 
     # Listar produtos
 
     elif opcao_selecionada == 'l':
+
+        os.system('clear')
+
+        # Tratativa de tentativa para listagem vazia
+
+        if len(lista_compras) == 0:
+            print('Sua lista está vazia')
+            continue
+
         for indice, produto in enumerate(lista_compras):
             print(indice, produto)
     
