53e10afa1f323c633590dd19da1d511ef46717a0
guilopesdev <lopesgui.pro@gmail.com>
2024-04-03 20:17:38 -0300

    Update aula102.py

--- aula102.py
+++ aula102.py
@@ -19,3 +19,26 @@ def recursiva (inicio=0, fim=10):
     return recursiva(inicio, fim)
 
 print(recursiva())
+
+#     # Caso base
+#     if inicio >= fim:
+#         return fim
+
+#     # Caso recursivo
+#     # contar até chegar ao final
+#     inicio += 1
+#     return recursiva(inicio, fim)
+
+
+# print(recursiva(0, 1001))
+
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
+print(factorial(100))
