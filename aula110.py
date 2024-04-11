c13dc6e0c5c3abbbd1d66e6d08f12b0d0c1793f6
guilopesdev <lopesgui.pro@gmail.com>
2024-04-11 20:07:39 -0300

    Create aula110.py

---
+++ aula110.py
@@ -0,0 +1,19 @@
+import os
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
+caminho_arquivo = 'teste.txt'
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+
+# os.unlink(caminho_arquivo)
+# os.rename(caminho_arquivo, 'teste-2.txt')
