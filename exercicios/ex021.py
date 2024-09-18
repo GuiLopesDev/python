fd32471418bf5358ef51892b327df5e6044206df
GuiLopesDev <155919320+GuiLopesDev@users.noreply.github.com>
2024-09-18 05:44:23 -0300

    ex021
    Começo de resolução do ex021

---
+++ exercicios/ex021.py
@@ -0,0 +1,29 @@
+'''
+Exercício - Lista de tarefas com desfazer e refazer
+   - Listar
+   - Desfazer a ultima operação (10, 9, 8, 7 ...)
+   - Refazer (7, 8, 9, 10)
+'''
+def lista_comandos(comando):
+    if comando == 'listar':
+        print(TodoList)
+    elif comando == 'desfazer':
+        desfazerList.append(TodoList(-1))
+        TodoList.pop()
+        print(TodoList)
+    elif comando == 'refazer':
+        TodoList.append(desfazerList(-1))
+        desfazerList.pop()
+        print(TodoList)
+    else:
+        TodoList.append(comando)
+        print(TodoList)
+    
+
+TodoList =[]
+desfazerList =[]
+
+while len(TodoList) < 10:
+    print('Comandos: listar, desfazer, refazer')
+    comando = input('Digite uma tarefa ou comando: ')
+    lista_comandos(comando)
