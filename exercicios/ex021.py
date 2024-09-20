2c58da2ccc402e13c3c3755577be15f316ceaf36
GuiLopesDev <155919320+GuiLopesDev@users.noreply.github.com>
2024-09-20 06:25:36 -0300

    Update ex021.py

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -4,28 +4,53 @@ Exercício - Lista de tarefas com desfazer e refazer
    - Desfazer a ultima operação (10, 9, 8, 7 ...)
    - Refazer (7, 8, 9, 10)
 '''
-def lista_comandos(comando):
-    if comando == 'listar':
-        print(*TodoList)
-    elif comando == 'desfazer':
-        desfazerList.append(TodoList(-1))
-        TodoList.pop()
-        print(*TodoList)
-    elif comando == 'refazer':
-        TodoList.append(desfazerList(-1))
-        desfazerList.pop()
-        print(*TodoList)
-    else:
-        TodoList.append(comando)
-        print(*TodoList)
-    
-
-TodoList =[
-        'TAREFAS:',
-        ]
+toDoList =[]
 desfazerList =[]
 
-while len(TodoList) < 10:
+def listar(toDoList):
+    print()
+    if not toDoList:
+        print('Nenhuma tarefa para listar')
+        return
+    
+    print('Tarefas: ')
+    for tarefa in toDoList:
+        print(f'\t{tarefa}')
+
+def desfazer(toDoList, desfazerList):
+    print()
+    if not toDoList:
+        print('Nenhuma tarefa para desfazer')
+        return
+    tarefa = toDoList.pop()
+    desfazerList.append(tarefa)
+
+def refazer(toDoList, desfazerList):
+    print()
+    if not toDoList:
+        print('Nenhuma tarefa para refazer')
+        return
+    tarefa = desfazerList.pop()
+    toDoList.append(tarefa)
+
+def adicionar(toDoList, comando):
+    print()
+    tarefa = comando.strip()
+    if not tarefa:
+        print('Você não digitou uma tarefa.')
+        return
+    toDoList.append(tarefa)
+    listar(toDoList)
+
+while True:
     print('Comandos: listar, desfazer, refazer')
     comando = input('Digite uma tarefa ou comando: ')
-    lista_comandos(comando)
+    
+    if comando == 'listar':
+        listar(toDoList)
+    elif comando == 'desfazer':
+        desfazer(toDoList, desfazerList)
+    elif comando == 'refazer':
+        refazer(toDoList, desfazerList)
+    else:
+        adicionar(toDoList, comando)
